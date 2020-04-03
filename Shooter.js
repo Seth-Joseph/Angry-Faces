a0dd61d3d7e616d78234004a80fc295d2100c4dd
Seth-Joseph <61108580+Seth-Joseph@users.noreply.github.com>
2020-04-03 19:03:31 +0300

    Add files via upload

---
+++ Shooter.js
@@ -0,0 +1,23 @@
+class Shooter extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/reeve.png");
+    this.Simg = loadImage("sprites/smoke.png");
+
+    this.path = []
+  }
+
+  display() {
+  
+    super.display();
+
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+
+    var position = [this.body.position.x,this.body.position.y];
+    this.path.push(position);
+    }
+    for(var i = 0;i < this.path.length; i ++){
+      image(this.Simg,this.path[i][0],this.path[i][1])
+    }
+  }
+}
